7d9542f01f076e1511076d4a928cba3afb1c69be
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-02-12 21:42:42 +0545

    [add]: config for .env variables

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+// environment_variables
+const nodeEnv = process.env.NODE_ENV;
+const portNumber = process.env.PORT_NUMBER || 9999;
+const clientBaseUrl = process.env.CLIENT_BASE_URL;
+const serverBaseUrl = process.env.SERVER_BASE_URL;
+const mongodbUrl = process.env.MONGODB_LOCAL_URL;
+const sessionSecret = process.env.SESSION_SECRET;
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+module.exports = {
+  nodeEnv,
+  portNumber,
+  clientBaseUrl,
+  serverBaseUrl,
+  mongodbUrl,
+  sessionSecret,
+  googleClientId,
+  googleClientSecret,
+};
