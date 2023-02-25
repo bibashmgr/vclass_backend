8aaac72277d801b1457b0afbe395910e8a5fb391
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-02-26 00:18:20 +0545

    [update]: validation for faculty-CRUD

--- routes/faculty.route.js
+++ routes/faculty.route.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { check } = require('express-validator');
 
 // controllers
 const {
@@ -9,12 +11,73 @@ const {
   deleteFaculty,
 } = require('../controllers/faculty.controller.js');
 
+// middlewares
+const { bodyValidation } = require('../middlewares/validation.middleware.js');
+
 const router = express.Router();
 
-router.post('/create', createFaculty);
+router.post(
+  '/create',
+  check('name').not().isEmpty().withMessage('Name is required').trim(),
+  check('semesters')
+    .isArray()
+    .withMessage('Semesters must be Array')
+    .custom((value) => {
+      if (value?.length === 0) {
+        return Promise.reject('Semesters are required');
+      }
+      return true;
+    }),
+  bodyValidation,
+  createFaculty
+);
+
 router.get('/', getFaculties);
-router.get('/:id', getFaculty);
-router.put('/:id', updateFaculty);
-router.delete('/:id', deleteFaculty);
+
+router.get(
+  '/:id',
+  check('id').custom((value) => {
+    if (!mongoose.isObjectIdOrHexString(value)) {
+      return Promise.reject('Invalid facultyId');
+    }
+    return true;
+  }),
+  bodyValidation,
+  getFaculty
+);
+
+router.put(
+  '/:id',
+  check('name').not().isEmpty().withMessage('Name is required').trim(),
+  check('semesters')
+    .isArray()
+    .withMessage('Semesters must be Array')
+    .custom((value) => {
+      if (value?.length === 0) {
+        return Promise.reject('Semesters are required');
+      }
+      return true;
+    }),
+  check('id').custom((value) => {
+    if (!mongoose.isObjectIdOrHexString(value)) {
+      return Promise.reject('Invalid facultyId');
+    }
+    return true;
+  }),
+  bodyValidation,
+  updateFaculty
+);
+
+router.delete(
+  '/:id',
+  check('id').custom((value) => {
+    if (!mongoose.isObjectIdOrHexString(value)) {
+      return Promise.reject('Invalid facultyId');
+    }
+    return true;
+  }),
+  bodyValidation,
+  deleteFaculty
+);
 
 module.exports = router;
