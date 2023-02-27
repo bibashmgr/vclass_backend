4eb830a09bca17a9dd9a5ca22c08c3fe12da690a
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-02-27 15:29:18 +0545

    [update]: middleware for validating user

--- middlewares/validation.middleware.js
+++ middlewares/validation.middleware.js
@@ -1,7 +1,11 @@
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 // utils
-const logger = require('../utils/logger');
+const logger = require('../utils/logger.js');
+
+// config
+const config = require('../config/config.js');
 
 const customFormat = ({ msg }) => {
   return `${msg}`;
@@ -21,4 +25,40 @@ const bodyValidation = (req, res, next) => {
   }
 };
 
-module.exports = { bodyValidation };
+const userValidation = (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, config.jwtSecret, (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            data: null,
+            success: false,
+            message: 'TOKEN EXPIRED',
+          });
+        } else {
+          req.userId = decoded.id;
+          next();
+        }
+      });
+    } catch (error) {
+      res.status(403).json({
+        data: null,
+        success: false,
+        message: 'ACCESS DENIED',
+      });
+    }
+  } else {
+    res.status(401).json({
+      data: null,
+      success: false,
+      message: 'TOKEN NOT FOUND',
+    });
+  }
+};
+
+module.exports = { bodyValidation, userValidation };
