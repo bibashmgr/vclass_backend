1bd6d557b0b088d9c5cfdb652f0e8409a4e08338
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-13 17:45:33 +0545

    [add]: controller for portal

---
+++ controllers/portal.controller.js
@@ -0,0 +1,15 @@
+const httpStatus = require('http-status');
+
+// utils
+const logger = require('../utils/logger.js');
+
+// models
+const portalModel = require('../models/portal.model.js');
+
+const getPortal = async (req, res) => {};
+
+const getPortalsByTeacher = async (req, res) => {};
+
+const assignTeacher = async (req, res) => {};
+
+modules.exports = {};
