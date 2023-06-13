ad8fac42e1b080615878b9be28ac9bc575edeacd
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-13 17:46:16 +0545

    [add]: controller for message

---
+++ controllers/message.controller.js
@@ -0,0 +1,10 @@
+const httpStatus = require('http-status');
+
+// utils
+const logger = require('../utils/logger.js');
+
+const createMessage = async (req, res) => {};
+
+const getMessages = async (req, res) => {};
+
+modules.exports = { createMessage, getMessages };
