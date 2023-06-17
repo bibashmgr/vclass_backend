45bb8c275205b276f86404402af415e54fd76635
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-17 18:43:49 +0545

    [update]: socket for chat

--- services/socket.service.js
+++ services/socket.service.js
@@ -5,6 +5,31 @@ const socketManager = (io) => {
   io.on('connection', (socket) => {
     logger.info(`${socket.id} connected`);
 
+    // portal
+    socket.on('join-portal', async (data) => {
+      const { subjectId, userInfo } = data;
+
+      socket.join(subjectId + userInfo.batch);
+      logger.info(`${socket.id} joined ${subjectId + userInfo.batch}`);
+    });
+
+    socket.on('leave-portal', (data) => {
+      const { subjectId, userInfo } = data;
+
+      socket.leave(subjectId + userInfo.batch);
+      logger.info(`${socket.id} left ${subjectId + userInfo.batch}`);
+    });
+
+    // messages
+    socket.on('send-message', (data) => {
+      const { subjectId, userInfo, messageInfo } = data;
+
+      socket.broadcast
+        .to(subjectId + userInfo.batch)
+        .emit('receive-message', { messageInfo });
+      logger.info(`${socket.id} sent message to ${subjectId + userInfo.batch}`);
+    });
+
     socket.on('disconnect', () => {
       logger.warn(`${socket.id} disconnected`);
     });
